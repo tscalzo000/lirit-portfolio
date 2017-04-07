264003ba972164e1c45ba1a07ddc4d61d447a2f7
vscalzo000 <vscalzo000@gmail.com>
2017-04-07 12:34:53 -0400

    Seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "test@test.com", password: "password")
+
+Category.create(name: "Production")
+Category.create(name: "Construction")
+Category.create(name: "Rendering")
+Category.create(name: "DrawingAndPainting")
+
+Resume.create(user_id: 1, url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Resume.pdf/page1-1200px-Resume.pdf.jpg")
+
+Bio.create(user_id: 1, text: "Your text here")
